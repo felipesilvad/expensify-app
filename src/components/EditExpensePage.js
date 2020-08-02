abc9c0c899e23eee99dcae5a333af5b5bd9bef57
Felipe <felipe.silvad@outlook.com>
2020-08-01 23:02:16 -0300

    AAAAAH

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.editExpense(this.props.expense.id, expense);
+    this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
   onRemove = () => {
-    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.startRemoveExpense({ id: this.props.expense.id });
     this.props.history.push('/');
   };
   render() {
     return (
       <div>
         <ExpenseForm
-          expense={this.props.expense}
+          expense={this.props.expense}a
           onSubmit={this.onSubmit}
         />
         <button onClick={this.onRemove}>Remove</button>
@@ -30,8 +30,8 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (data) => dispatch(removeExpense(data))
+  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
+
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
@@ -11,7 +12,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       {numeral(amount / 100).format('$0,0.00')}
       -
       {moment(createdAt).format('MMMM Do, YYYY')}
-      </p>
+    </p>
   </div>
 );
 
